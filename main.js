5a66b5aaad67144a3bd5c5b5a02409ae6004b1a3
Niccolo Montini <niccolo@productbuilder.ai>
2019-04-03 10:45:54 +0200

    nieuwe versie

---
+++ main.js
@@ -0,0 +1,75 @@
+   // Variabele maken voor de antwoord-buttons
+            const button1 = document.getElementById("button1");
+            const button2 = document.getElementById("button2");
+
+            // Counter begint bij 1, aangezien de eerste vraag direct al wordt ingeladen.
+            var counter = 1;
+
+            // Zodat de punten telling begint bij de punten van de eerste vraag
+            var offsetCounter = 0;
+
+            // Variabele maken voor de scores
+            var valuation = document.getElementById("valu");
+            var happiness = document.getElementById("happ");
+
+            // Tijdelijke scoren begint op 0, hierna wordt de score behaald door het antwoorden van de vraag, toegevoegd.
+            var temporaryScoreVal = 0;
+            var temporaryScoreHap = 0;
+
+            // Eerste vraag standaard inladen
+            var firstQuestion = document.getElementById("question");
+            var firstAnswerA = document.getElementById("A");
+            var firstAnswerB = document.getElementById("B");
+            firstQuestion.innerHTML = window.cards[0].description;
+            firstAnswerA.innerHTML = window.cards[0].choices.a.label;
+            firstAnswerB.innerHTML = window.cards[0].choices.b.label;
+
+            // Hier begint de functie die wordt uitgevoerd alsje op antwoord-knop A drukt.
+            button1.addEventListener("click", function(){
+             
+             // Checken of je score boven de 0 is.
+             if(temporaryScoreHap>=0 || temporaryScoreVal >=0){
+
+            // De ...e vraag en antwoorden worden ingeladen (... = counter)
+            document.getElementById("question").innerHTML = window.cards[counter].description;
+            document.getElementById("A").innerHTML = window.cards[counter].choices.a.label;
+            document.getElementById("B").innerHTML = window.cards[counter].choices.b.label;
+
+            // De tijdelijke score + de score behaald door het antwoorden van optie A
+            temporaryScoreVal += window.cards[offsetCounter].choices.a.values.valuation;
+            temporaryScoreHap += window.cards[offsetCounter].choices.a.values.happiness;
+            console.log(temporaryScoreVal,temporaryScoreHap);
+            valuation.innerHTML = temporaryScoreVal;
+            happiness.innerHTML = temporaryScoreHap;
+            counter ++;
+            offsetCounter ++;
+            }else{
+                    console.log("ur dead");
+            }
+            });
+        
+            // Hier begint de functie die wordt uitgevoerd alsje op antwoord-knop B drukt.
+            button2.addEventListener("click", function(){
+           // Checken of je score boven de 0 is.
+            if(temporaryScoreHap>=0 || temporaryScoreVal >=0){
+
+            document.getElementById("question").innerHTML = window.cards[counter].description;
+            document.getElementById("A").innerHTML = window.cards[counter].choices.a.label;
+            document.getElementById("B").innerHTML = window.cards[counter].choices.b.label;
+
+           
+
+            // De tijdelijke score + de score behaald door het antwoorden van optie B   
+            temporaryScoreVal += window.cards[offsetCounter].choices.b.values.valuation;
+            temporaryScoreHap += window.cards[offsetCounter].choices.b.values.happiness; 
+            console.log(temporaryScoreVal,temporaryScoreHap);   
+            valuation.innerHTML =  temporaryScoreVal;
+            happiness.innerHTML = valuation.innerHTML;
+            counter ++;
+            offsetCounter ++;
+            }else{
+                    console.log("ur dead");
+            }
+            });
+
+            
